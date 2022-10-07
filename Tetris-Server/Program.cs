41ef98dc2663d19d7160234dec4384086816a9c1
LarochelleJ <larochelle.jason@live.fr>
2022-10-07 17:43:09 -0400

    Ajoutez des fichiers projet.

---
+++ Tetris-Server/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Net;
+using System.Text;
+using Mina.Core.Service;
+using Mina.Filter.Codec;
+using Mina.Filter.Codec.TextLine;
+using Mina.Filter.Logging;
+using Mina.Transport.Socket;
+
+namespace Tetris_Server {
+    class Program {
+        private static readonly int port = 2121;
+        private static bool isRunning = true;
+        private static SessionHandler sHandler = new SessionHandler();
+        private static IoAcceptor acceptor;
+        public static bool debug = false;
+
+        static void Main(string[] args) {
+            acceptor = new AsyncSocketAcceptor();
+
+
+            acceptor.FilterChain.AddLast("logger", new LoggingFilter());
+            acceptor.FilterChain.AddLast("codec", new ProtocolCodecFilter(new TextLineCodecFactory(Encoding.UTF8)));
+
+            acceptor.Handler = sHandler;
+
+            acceptor.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), port));
+
+            DisplayHomeMessage();
+
+            while (isRunning) {
+                string consoleMessage = Console.ReadLine();
+                string[] messageArgs = consoleMessage.Split(" ");
+                string cmd = messageArgs.Length > 0 ? messageArgs[0] : "";
+                switch (cmd.ToLower()) {
+                    case "shutdown":
+                        isRunning = false;
+                        break;
+                    case "sendall":
+                        string messageToSend = consoleMessage.Substring(cmd.Length+1);
+                        Console.WriteLine("Sent {0} to everyone", messageToSend);
+                        sHandler.SendAll(messageToSend);
+                        break;
+                    case "debug":
+                        debug = !debug;
+                        if (debug) Console.WriteLine("Debug mode enabled"); else Console.WriteLine("Debug mode disabled");
+                        break;
+                    case "clear":
+                        Console.Clear();
+                        DisplayHomeMessage();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private static void DisplayHomeMessage() {
+            Console.WriteLine("Listening on " + acceptor.LocalEndPoint + Environment.NewLine);
+            Console.WriteLine("Available commands");
+            Console.WriteLine("shutdown - Shutdown the server");
+            Console.WriteLine("sendall - Send a message / packet to every sessions");
+            Console.WriteLine("debug - Display debugging informations");
+            Console.WriteLine("clear - Clear the console" + Environment.NewLine);
+        }
+
+    }
+}
---
+++ Tetris-Server/SessionHandler.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mina.Core.Service;
+using Mina.Core.Session;
+
+namespace Tetris_Server {
+    class SessionHandler : IoHandlerAdapter {
+        // Session pool
+        Dictionary<IoSession, Boolean> sessions = new Dictionary<IoSession, Boolean>();
+
+        public override void SessionCreated(IoSession session) {
+            sessions[session] = true;
+        }
+
+        public override void MessageReceived(IoSession session, Object message) {
+            if (Program.debug) {
+                Console.WriteLine($"Packet received: {message}");
+            }
+            parsePacket((string)message);
+        }
+
+        public override void SessionClosed(IoSession session) {
+            sessions.Remove(session);
+        }
+
+        public void SendAll(string message) {
+            foreach (IoSession session in sessions.Keys) {
+                session.Write(message);
+            }
+        }
+
+
+        public override void ExceptionCaught(IoSession session, Exception cause) {
+            Console.WriteLine("Unexpected exception." + cause);
+            session.Close(true);
+        }
+
+        private void parsePacket(string packet) {
+            string[] args = packet.Split("|");
+            if (args.Length > 0) {
+                switch (args[0]) {
+                    case "p":
+                        int shape_id = int.Parse(args[1]);
+                        SendAll("p|" + shape_id);
+                        break;
+                    case "r":
+                        SendAll("r|");
+                        break;
+                    case "l":
+                        SendAll("l|");
+                        break;
+                    case "d":
+                        SendAll("d|");
+                        break;
+                    case "ro":
+                        SendAll("ro|");
+                        break;
+                    case "sd":
+                        SendAll("sd|");
+                        break;
+                    case "sb":
+                        SendAll("sb|");
+                        break;
+                }
+            }
+        }
+    }
+}
